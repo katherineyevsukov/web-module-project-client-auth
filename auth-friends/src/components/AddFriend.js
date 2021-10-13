9e725a5196e9e0577f7a22899e0281e24be9660c
Kat <kyevsukov@gmail.com>
2021-10-13 09:52:39 -0400

    pushed back to freinds list upon adding freind

--- auth-friends/src/components/AddFriend.js
+++ auth-friends/src/components/AddFriend.js
@@ -10,50 +10,70 @@ const initialFormValues = {
 };
 
 const AddFriend = (props) => {
-    console.log(props)
+  console.log(props);
   const [formValues, setFormValues] = useState(initialFormValues);
-  const history = useHistory()
- 
+  const history = useHistory();
+
   const handleChange = (e) => {
-      setFormValues({
-          ...formValues,
-          [e.target.name]: e.target.value
-      })
-  }
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const createNewFriend = () => {
-      const newFriend = {...formValues, id: Date.now}
-      return newFriend
-  }
+    const newFriend = { ...formValues, id: Date.now };
+    return newFriend;
+  };
 
   const handleSubmit = async (e) => {
-      e.preventDefault()
-      const token = localStorage.getItem("token")
-      const friend = createNewFriend()
-      try { const res = await axios.post('http://localhost:5000/api/friends', friend, {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    const friend = createNewFriend();
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/friends",
+        friend,
+        {
           headers: {
-              authorization: token
-          }
-      })
-      console.log(res)
+            authorization: token,
+          },
+        }
+      );
+      props.setFriends(res.data)
+      history.push('/friends')
     } catch (err) {
-        console.error(err)
+      console.error(err);
     }
-
-  }
+  };
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" value={formValues.name} onChange={handleChange}/>
+        <input
+          type="text"
+          name="name"
+          value={formValues.name}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Age
-        <input type="number" name="age" value={formValues.age} onChange={handleChange}/>
+        <input
+          type="number"
+          name="age"
+          value={formValues.age}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Email:
-        <input type="email" name="email" value={formValues.email} onChange={handleChange}/>
+        <input
+          type="email"
+          name="email"
+          value={formValues.email}
+          onChange={handleChange}
+        />
       </label>
       <button>Submit</button>
     </form>
